a431cd64b0cac81a5d525786002a0cdb16126a4a
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2024-06-26 09:20:23 +0530

    FIX: bugs

--- theme.config.jsx
+++ theme.config.jsx
@@ -6,6 +6,12 @@ const GITHUB_REPO_STAR = <GithubStar />;
 export default {
   primaryHue: 80,
   logo: <span className="text-3xl">IntentJs</span>,
+  toc: {
+    backToTop: true,
+  },
+  chat: {
+    link:'https://google.com',
+  },
   project: {
     link: "https://github.com/intentjs/core",
     icon: (
