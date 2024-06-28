564910fc4c876bb4bb9e063e1b88221c0020ce44
ocmodi21 <ocmodi2117@gmail.com>
2024-06-28 16:57:09 +0530

    refactor: footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,50 @@
 import React from "react";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { BsTwitterX } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa6";
+import { PiYoutubeLogoFill } from "react-icons/pi";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col w-full px-10">
-      <div className="flex flex-col w-full">
-        <div className="grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 xl:grid-rows-1 mt-5 gap-5">
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold">Features</span>
-            <span className="mt-2 text-sm">Home</span>
-            <span className="mt-2 text-sm">Docs</span>
-            <span className="mt-2 text-sm">Blog</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold">Support</span>
-            <span className="mt-2 text-sm">tryhanalabs.com</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold">Support</span>
-            <span className="mt-2 text-sm">tryhanalabs.com</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold">Social Link</span>
-            <span className="mt-2 text-sm">Github</span>
-            <span className="text-sm">Twitter</span>
-            <span className="text-sm">LinkedIn</span>
-          </div>
+    <div className="flex flex-col justify-center items-center w-full pt-12 pb-5">
+      <div className="flex flex-row gap-5">
+        <div className="rounded-full bg-[#fff] p-2 cursor-pointer">
+          <FaSquareInstagram size={20} color="black" />
+        </div>
+        <div className="rounded-full bg-[#fff] p-2 cursor-pointer">
+          <BsTwitterX size={20} color="black" />
+        </div>
+        <div className="rounded-full bg-[#fff] p-2 cursor-pointer">
+          <FaLinkedin size={20} color="black" />
+        </div>
+        <div className="rounded-full bg-[#fff] p-2 cursor-pointer">
+          <PiYoutubeLogoFill size={20} color="black" />
         </div>
       </div>
 
-      <div className="flex justify-center mt-10">
-        <span className="font-semibold">
+      <div className="flex flex-col text-center md:flex-row flex-wrap gap-6 md:gap-10 mt-6">
+        <span className="text-md text-[#A3A3A3] font-semibold cursor-pointer dark:hover:text-[#fff]">
+          Home
+        </span>
+        <span className="text-md text-[#A3A3A3] font-semibold cursor-pointer dark:hover:text-[#fff]">
+          News
+        </span>
+        <span className="text-md text-[#A3A3A3] font-semibold cursor-pointer dark:hover:text-[#fff]">
+          About
+        </span>
+        <span className="text-md text-[#A3A3A3] font-semibold cursor-pointer dark:hover:text-[#fff]">
+          Contact Us
+        </span>
+        <span className="text-md text-[#A3A3A3] font-semibold cursor-pointer dark:hover:text-[#fff]">
+          Our Team
+        </span>
+      </div>
+
+      <div className="mt-16">
+        <span className="text-sm text-[#A3A3A3] font-bold cursor-pointer">
           {new Date().getFullYear()} ©{" "}
           <a href="https://tryintent.com" target="_blank">
-            Intent
+            IntentJs. All rights reserved.
           </a>
         </span>
       </div>
