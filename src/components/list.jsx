66405c04aa0bc32ed3fdb077d554b6988c2ab75b
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2024-06-29 15:45:38 +0530

    fix bugs and update configuration document

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const ListComponent = ({ items }) => {
   return (
     <>
-      <div class="grid grid-cols-4 gap-3 mx-auto py-2">
+      <div className="grid grid-cols-4 gap-3 mx-auto py-2">
         {items.map((i) => (
           <div className="underline text-primary">
             <Link href={i.href || ""}>{i.label}</Link>
